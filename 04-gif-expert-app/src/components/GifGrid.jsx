eee312d9b2783224a7a18fc823af6ff2d12f5734
Cesar Morales <cesar.aml1891@gmail.com>
2025-06-12 07:08:13 -0500

    Trabajando con IsLoading

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,20 +1,21 @@
 import { GifItem } from "./GifItem";
 import { useFetchGifs } from "../hooks/useFetchGifs";
 
-export const GifGrid = ({category}) => {
+export const GifGrid = ({ category }) => {
     const { images, isLoading } = useFetchGifs(category);
 
     return (
         <>
             <h3>{category}</h3>
+            {isLoading && 
+                <h2>Cargando...</h2>}
+            
 
-            <div key={category} className="card-grid">
-                {
-                    images.map( image => {
-                        return <GifItem key={image.id} { ...image } />
-                    })
-                }
-            </div>
+            {!isLoading && 
+                images.map(image => {
+                    return <GifItem key={image.id} {...image} />
+                })
+            }
         </>
     )
 }
