1fb286e4dad9e9dab258912b2c4220b3d40c5567
Cesar Morales <cesar.aml1891@gmail.com>
2026-02-01 22:12:56 -0500

    39-impresion-variables

--- 02-first-steps/src/MyAwesomeApp.tsx
+++ 02-first-steps/src/MyAwesomeApp.tsx
@@ -1,8 +1,19 @@
-export function MyAwesomeApp() {
+const firstName = 'Cesar';
+const lastName = 'Morales';
+const favoriteGames = ['game1', 'game2', 'game3'];
+const isActive = false;
+const address = {
+    zipCode: 'ABC-123',
+    country: 'Canada'
+};
+export const MyAwesomeApp = () => {
     return (
         <>
-            <h1>Cesar</h1>
-            <h3>Morales</h3>
+            <h1>{firstName}</h1>
+            <h3>{lastName}</h3 >
+            <p>{favoriteGames.join(', ')}</p>
+            <h1>{isActive ? 'Activo' : 'No activo'}</h1>
+            <p>{JSON.stringify(address)}</p>
         </>
     );
 }
