1b0a5d3e35a9d1b830e3a0beadd4411ca4b12aa5
Cesar Morales <cesar.aml1891@gmail.com>
2026-02-11 22:30:51 -0500

    43.- Mostrar listados de elementos

--- 02-first-steps/src/FirstStepsApp.tsx
+++ 02-first-steps/src/FirstStepsApp.tsx
@@ -1,12 +1,22 @@
 import { ItemCounter } from "./shopping-cart/ItemCounter";
 
+interface ItemCart {
+    id: number,
+    productName: string,
+    quantity: number | undefined
+};
+
+const itemsInCart: ItemCart[] = [
+    { id: 1, productName: 'Nintendo 1', quantity: 10 },
+    { id: 2, productName: 'Nintendo 2', quantity: 20 },
+    { id: 3, productName: 'Nintendo 3', quantity: undefined },
+];
+
 export function FirstStepsApp() {
     return (
         <>
             <h1>Carrito de compras</h1>
-            <ItemCounter name={"Nintendo 1"} quantity={10} />
-            <ItemCounter name={"Nintendo 2"} quantity={20} />
-            <ItemCounter name={"Nintendo 3"} quantity={undefined} />
+            {itemsInCart.map(({ id, productName, quantity }) => <ItemCounter key={id} name={productName} quantity={quantity} />)}
         </>
     );
 }
--- 02-first-steps/src/shopping-cart/ItemCounter.tsx
+++ 02-first-steps/src/shopping-cart/ItemCounter.tsx
@@ -3,8 +3,8 @@ import { useState } from 'react'
 interface Props {
     name: string,
     //Valor opcional
-    quantity?: number,                  //Forma 1: quantity es opcional
-    // quantity: number | undefined,    //Forma 2: quantity es obligatorio, al meno pasarlo com undefined
+    //quantity?: number,                  //Forma 1: quantity es opcional
+    quantity: number | undefined,    //Forma 2: quantity es obligatorio, al meno pasarlo com undefined
 }
 
 export const ItemCounter = ({ name, quantity }: Props) => {
@@ -12,7 +12,7 @@ export const ItemCounter = ({ name, quantity }: Props) => {
     return (
         <section style={{ display: 'flex', alignItems: 'center', gap: '10px', marginTop: '10px' }}>
             <span style={{ width: 150 }}>{name}</span>
-            <button onClick={() => setCounter(counter++)}>+1</button>
+            <button onClick={() => setCounter(++counter)}>+1</button>
             <span>{counter}</span>
             <button onClick={() => setCounter(--counter)}>-1</button>
         </section>
