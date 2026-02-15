9101467f80d548b148b16818461b29ada4d69573
Cesar Morales <cesar.aml1891@gmail.com>
2026-02-15 10:25:58 -0500

    53.- Agrupar pruebas similares

--- 02-first-steps/src/helpers/math.helper.test.ts
+++ 02-first-steps/src/helpers/math.helper.test.ts
@@ -1,7 +1,47 @@
-import { expect, test } from "vitest";
-import { add } from "./math.helper";
+import { describe, expect, test } from "vitest";
+import { add, multiply, substract } from "./math.helper";
 
-test('should add two positives numbers', () => {
-    const result = add(1, 3);
-    expect(result).toBe(4);
+describe('add', () => {
+    test('should add two positives numbers', () => {
+        // 1.- Arrange
+        const a = 1;
+        const b = 3;
+        // 2.- Act
+        const result = add(a, b);
+        // 2.- Assert
+        expect(result).toBe(a + b);
+    })
+
+})
+describe('substract', () => {
+    test('should substract two positives numbers', () => {
+        // 1.- Arrange
+        const a = 4;
+        const b = 3;
+        // 2.- Act
+        const result = substract(a, b);
+        // 2.- Assert
+        expect(result).toBe(a - b);
+    })
+    test('should substract two negatives numbers', () => {
+        // 1.- Arrange
+        const a = 1;
+        const b = 3;
+        // 2.- Act
+        const result = substract(a, b);
+        // 2.- Assert
+        expect(result).toBe(a - b);
+    })
+
+})
+describe('multiply', () => {
+    test('should add two positives numbers', () => {
+        // 1.- Arrange
+        const a = 1;
+        const b = 3;
+        // 2.- Act
+        const result = multiply(a, b);
+        // 2.- Assert
+        expect(result).toBe(a * b);
+    })
 })
