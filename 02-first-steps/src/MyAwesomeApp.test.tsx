a44a356b7eac2f4994ad1f0a80a0c1a95327a94c
Cesar Morales <cesar.aml1891@gmail.com>
2026-02-15 23:13:09 -0500

    55.- Buscar elementos en el componente renderizado

--- 02-first-steps/src/MyAwesomeApp.test.tsx
+++ 02-first-steps/src/MyAwesomeApp.test.tsx
@@ -1,4 +1,4 @@
-import { describe, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { render, screen } from '@testing-library/react'
 import { MyAwesomeApp } from "./MyAwesomeApp";
 
@@ -6,6 +6,22 @@ describe("MyAwesomeApp", () => {
     test('should render firstname and lastname', () => {
         const { container } = render(<MyAwesomeApp />)
         // console.log(container.innerHTML);
-        screen.debug();
+        const h1 = container.querySelector('h1');
+        const h3 = container.querySelector('h3');
+        // console.log(h1?.innerHTML);
+        // console.log(h3?.innerHTML);
+        // Es igual al texto
+        expect(h1?.innerHTML).toBe('Cesar');
+        expect(h3?.innerHTML).toBe('Morales');
+        // Contiene texto
+        // expect(h1?.innerHTML).toContain('Cesar');
+        // expect(h3?.innerHTML).toContain('Morales');
+    })
+    test('should render firstname and lastname - screen', () => {
+        render(<MyAwesomeApp />)
+        const h1 = screen.getAllByRole('heading', {
+            level: 1,
+        });
+        expect(h1[0]?.innerHTML).toContain('Cesar');
     })
 })
