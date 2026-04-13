87d0d46b94c71ce12afdda779b66f86c0686b987
Cesar Morales Dev <cesar.morales.dev@gmail.com>
2026-04-12 21:30:58 -0500

    85.- Problema que resuelve un 'custom hook'

---
+++ 03-gift-app/src/counter/components/MyCounterApp.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const styleContainer: React.CSSProperties = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '50%',
+    height: '80vh',
+    margin: '0 auto',
+};
+const styleContainerButtons: React.CSSProperties = {
+    display: 'flex',
+    width: '30%',
+    flexDirection: 'column',
+    gap: '10px',
+};
+const styleButton: React.CSSProperties = {
+    width: '100%',
+    height: '3em',
+};
+
+export const MyCounterApp = () => {
+    const [counter, setCounter] = useState(5);
+
+    const handleAdd = () => setCounter(counter + 1);
+    const handleSubtract = () => setCounter(prevState => prevState - 1);
+    const handleReset = () => setCounter(5);
+
+    return (
+        <div style={styleContainer}>
+            <h2>counter: {counter}</h2>
+            <div style={styleContainerButtons}>
+                <button style={styleButton} onClick={handleAdd}>+1</button>
+                <button style={styleButton} onClick={handleSubtract}>-1</button>
+                <button style={styleButton} onClick={handleReset}>Reset</button>
+            </div>
+        </div>
+    )
+}
