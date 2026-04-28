abe7ca148ce7d55a1353f5e2c8a584cc29bf049b
Cesar Morales Dev <cesar.morales.dev@gmail.com>
2026-04-27 21:09:12 -0500

    119.- Tipado estricto en useState

--- 04-hooks-app/src/TrafficLight.tsx
+++ 04-hooks-app/src/TrafficLight.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 
-const color = {
+const colors = {
     red: 'bg-red-500 animate-pulse',
     yellow: 'bg-yellow-500 animate-pulse',
     green: 'bg-green-500 animate-pulse',
+    blue: 'bg-blue-500 animate-pulse',
+    reset: 'bg-gray-500 animate-pulse',
 };
 
 const colorValue = {
@@ -12,18 +14,21 @@ const colorValue = {
     VERDE: 'green',
 }
 
+// type TrafficLightColors = 'red' | 'yellow' | 'green' | 'blue';
+type TrafficLightColors = keyof typeof colors;
+
 export const TrafficLight = () => {
-    const [light, setLight] = useState('');
+    const [light, setLight] = useState<TrafficLightColors>('reset');
 
     const onClickVariable = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         let element : HTMLButtonElement = event.target as HTMLButtonElement;
-        let textElement: string = element.childNodes[0].textContent?.toUpperCase() || '';        
-        textElement === 'RESET' ? setLight('') : setLight(colorValue[textElement as keyof typeof colorValue] || '');
+        let textElement: string = element.childNodes[0].textContent?.toLocaleLowerCase() || '';        
+        setLight(textElement as TrafficLightColors);
     }
 
-    const handleColorChange = (color: string) => {
+    const handleColorChange = (color: TrafficLightColors) => {
         // Con este codigo obtener el valor previo del estado y el valor actual que se va a establecer
-        setLight((prev: string) => {
+        setLight((prev: TrafficLightColors) => {
             console.log("Valor previo: ", {prev});
             console.log("Valor actual: ", {color});
             return color;
@@ -33,35 +38,39 @@ export const TrafficLight = () => {
     return (
         <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
             <div className="flex flex-col items-center space-y-8">
-                <div className={`w-32 h-32 ${light === 'red' ? color.red : 'bg-gray-500'} rounded-full`}></div>
-                <div className={`w-32 h-32 ${light === 'yellow' ? color.yellow : 'bg-gray-500'} rounded-full`}></div>
-                <div className={`w-32 h-32 ${light === 'green' ? color.green : 'bg-gray-500'} rounded-full`}></div>
+                <div className={`w-32 h-32 ${light === 'red' ? colors.red : 'bg-gray-500'} rounded-full`}></div>
+                <div className={`w-32 h-32 ${light === 'yellow' ? colors.yellow : 'bg-gray-500'} rounded-full`}></div>
+                <div className={`w-32 h-32 ${light === 'green' ? colors.green : 'bg-gray-500'} rounded-full`}></div>
 
                 {/* Botón para cambiar el estado de la luz */}
                 <div className="flex gap-2">
                     <button
+                        // onClick={onClickVariable}
                         onClick={() => handleColorChange('red')}
                         className="bg-red-500 text-white px-4 py-2 rounded-md cursor-pointer">
-                        Rojo
+                        Red
                     </button>
                     <button
+                        // onClick={onClickVariable}
                         onClick={() => handleColorChange('yellow')}
                         className="bg-yellow-500 text-white px-4 py-2 rounded-md cursor-pointer">
-                        Amarillo
+                        Yellow
                     </button>
                     <button
+                        // onClick={onClickVariable}
                         onClick={() => handleColorChange('green')}
                         className="bg-green-500 text-white px-4 py-2 rounded-md cursor-pointer">
-                        Verde
+                        Green
                     </button>
                 </div>
-                {/* <div className="flex gap-2">
+                <div className="flex gap-2">
                     <button
-                        onClick={onClick}
+                        // onClick={onClickVariable}
+                        onClick={() => handleColorChange('green')}
                         className="bg-gray-500 text-white px-4 py-2 rounded-md cursor-pointer">
                         Reset
                     </button>
-                </div> */}
+                </div>
             </div>
         </div>
     );
