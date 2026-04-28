3d1b1ea1fd4e31e7451e64e45ecb944dfe595a1e
Cesar Morales Dev <cesar.morales.dev@gmail.com>
2026-04-28 00:09:03 -0500

    126.- useRef - Valor que no dispara re-render

--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -6,6 +6,7 @@ import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
 import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
 import { TrafficLight } from './01-useState/TrafficLight'
 import { PokemonPage } from './03-examples/PokemonPage'
+import { FocusScreen } from './04-useRef/FocusScreen'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithEffect /> */}
     {/* <TrafficLightWithHook /> */}
-    <PokemonPage />
+    {/* <PokemonPage /> */}
+    <FocusScreen />
   </StrictMode>,
 )
