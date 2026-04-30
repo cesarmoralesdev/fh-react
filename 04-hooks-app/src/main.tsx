f24d9a45dfcda6b48ddf85c78f308d747cfd9c66
Cesar Morales Dev <cesar.morales.dev@gmail.com>
2026-04-29 22:54:08 -0500

    131.- Continuacion de proyecto - useReducer

--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -7,6 +7,7 @@ import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
 import { TrafficLight } from './01-useState/TrafficLight'
 import { PokemonPage } from './03-examples/PokemonPage'
 import { FocusScreen } from './04-useRef/FocusScreen'
+import { TasksApp } from './05-useReducer/TaskApp'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <TrafficLightWithEffect /> */}
     {/* <TrafficLightWithHook /> */}
     {/* <PokemonPage /> */}
-    <FocusScreen />
+    {/* <FocusScreen /> */}
+    <TasksApp />
   </StrictMode>,
 )
