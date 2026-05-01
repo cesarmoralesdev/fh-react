e363b8a872422e75ccd2bdac829eb1cb6e6190d6
Cesar Morales Dev <cesar.morales.dev@gmail.com>
2026-05-01 12:59:23 -0500

    139.- Validadores de objetos - Zod

--- 04-hooks-app/src/05-useReducer/reducer/tasksReducer.tsx
+++ 04-hooks-app/src/05-useReducer/reducer/tasksReducer.tsx
@@ -1,3 +1,6 @@
+
+import * as z from "zod";
+
 interface Todo {
     id: number;
     text: string;
@@ -16,6 +19,19 @@ export type TaskAction =
     | { type: 'TOGGLE_TODO', payload: number }
     | { type: 'DELETE_TODO', payload: number }
 
+const TodoSchema = z.object({
+    id: z.number(),
+    text: z.string(),
+    completed: z.boolean(),
+});
+
+const TaskStateSchema = z.object({
+    todos: z.array(TodoSchema),
+    length: z.number(),
+    completed: z.number(),
+    pending: z.number(),
+});
+
 export const getTasksInitialState = (): TaskState => {
     const localStorageState = localStorage.getItem('tasks-state')
     if (!localStorageState) {
@@ -26,8 +42,21 @@ export const getTasksInitialState = (): TaskState => {
             length: 0,
         };
     }
+
+    // Validar mediante Zod
+    const result = TaskStateSchema.safeParse(JSON.parse(localStorageState));
+    if (result.error) {
+        console.log(result.error);
+        return {
+            todos: [],
+            completed: 0,
+            pending: 0,
+            length: 0,
+        };
+    }
+
     return {
-        ...JSON.parse(localStorageState),
+        ...result.data,
     };
 }
 
