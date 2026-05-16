2e65876cac84d5d1815778396bb76a0e1d9cee43
Cesar Morales Dev <cesar.morales.dev@gmail.com>
2026-05-16 17:45:07 -0500

    166.- Contexto de usuario - informacion global

--- 04-hooks-app/src/09-useContext/context/UserContext.tsx
+++ 04-hooks-app/src/09-useContext/context/UserContext.tsx
@@ -1,5 +1,38 @@
-import { type PropsWithChildren } from "react"
+import type { User } from "@/08-use-suspense/api/get-user.action"
+import { createContext, useState, type PropsWithChildren } from "react"
 
+type AuthStatus = 'checking' | 'authenticated' | 'not-authenticated';
+
+interface UserContextProps {
+    //state
+    authStatus: AuthStatus
+    user: User | null
+
+    //Method
+    login: (userId: number) => boolean
+    logout: () => void
+}
+
+export const UserContext = createContext({} as UserContextProps);
+
+//HOC
 export const UserContextProvider = ({ children }: PropsWithChildren) => {
-    return <>{children}</>
+    const [authStatus, setAuthStatus] = useState<AuthStatus>('checking');
+    const [user, setUser] = useState<User|null>();
+
+    const handleLogin = (userId: number) => {
+        console.log({userId});
+        return true;
+    }
+    const handleLogout = () => {
+        console.log('logout');
+        
+    }
+
+    return <UserContext value={{
+        authStatus: 'checking',
+        user: null,
+        login: handleLogin,
+        logout: handleLogout
+    }}>{children}</UserContext>
 }
