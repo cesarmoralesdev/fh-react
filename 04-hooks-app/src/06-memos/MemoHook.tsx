bd58a0bf65fbcb633d8c691319117d5fd312ddc1
Cesar Morales <cesar.aml1891@gmail.com>
2026-05-16 03:49:32 -0500

    149.- Continuacion de aplicacion

---
+++ 04-hooks-app/src/06-memos/MemoHook.tsx
@@ -0,0 +1,25 @@
+import { MyTitle } from './ui/MyTitle'
+import { MySubtitle } from './ui/MySubtitle'
+import { useState } from 'react'
+
+export const MemoHook = () => {
+    const [title, setTitle] = useState('hola');
+    const [subtitle, setSubtitle] = useState('mundo');
+
+    return (
+        <div className='bg-gradient flex flex-col gap-4'>
+            <h1 className='text-2xl font-thin text-white'>Memo App</h1>
+
+            <MyTitle title={title} />
+            <MySubtitle subtitle={subtitle} />
+
+            <button className='bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer'>
+                Cambiar titulo
+            </button>
+            <button className='bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer'>
+                Cambiar subtitulo
+            </button>
+
+        </div>
+    )
+}
