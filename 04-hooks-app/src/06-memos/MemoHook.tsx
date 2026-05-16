cd53d818ab05b2bc46bbb7edf317bb83b622de04
Cesar Morales <cesar.aml1891@gmail.com>
2026-05-16 04:29:52 -0500

    151.- useCallback - Memorizacion de funciones

--- 04-hooks-app/src/06-memos/MemoHook.tsx
+++ 04-hooks-app/src/06-memos/MemoHook.tsx
@@ -1,22 +1,30 @@
 import { MyTitle } from './ui/MyTitle'
 import { MySubtitle } from './ui/MySubtitle'
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
+
+const handleMyAPICallFueraComponente = (myValue: string) => {
+    console.log('Llamar a API fuera de componente', myValue);
+}
 
 export const MemoHook = () => {
     const [title, setTitle] = useState('hola');
     const [subtitle, setSubtitle] = useState('mundo');
+
+    const handleMyAPICall = useCallback(() => {
+        console.log('Llamar a API', subtitle);
+    }, [subtitle])
 
     return (
         <div className='bg-gradient flex flex-col gap-4'>
             <h1 className='text-2xl font-thin text-white'>Memo App</h1>
 
             <MyTitle title={title} />
-            <MySubtitle subtitle={subtitle} />
+            <MySubtitle subtitle={subtitle} callMyAPI={handleMyAPICall} callMyAPIFueraComponente={handleMyAPICallFueraComponente} />
 
             <button
                 className='bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer'
                 onClick={() => setTitle('Hello, ' + new Date().getTime())}
-                >
+            >
                 Cambiar titulo
             </button>
             <button
--- 04-hooks-app/src/06-memos/ui/MySubtitle.tsx
+++ 04-hooks-app/src/06-memos/ui/MySubtitle.tsx
@@ -2,8 +2,10 @@ import { memo } from "react";
 
 interface Props {
     subtitle: string
+    callMyAPI: () => void
+    callMyAPIFueraComponente: (myValue: string) => void
 }
-export const MySubtitle = memo(({ subtitle }: Props) => {
+export const MySubtitle = memo(({ subtitle, callMyAPI, callMyAPIFueraComponente }: Props) => {
 
     console.log('MySubtitle re-render');
 
@@ -11,8 +13,18 @@ export const MySubtitle = memo(({ subtitle }: Props) => {
         <>
             <h6 className="text-2xl font-bold">{subtitle}</h6>
 
-            <button className="bg-indigo-500 text-white px-2 py-1 rounded-md cursor-pointer">
-                Llamar a funcion
+            <button
+                className="bg-indigo-500 text-white px-2 py-1 rounded-md cursor-pointer"
+                onClick={callMyAPI}
+            >
+                Llamar a funcion con funcion dentro de componente
+            </button>
+
+            <button
+                className="bg-indigo-500 text-white px-2 py-1 rounded-md cursor-pointer"
+                onClick={() => callMyAPIFueraComponente(subtitle)}
+            >
+                Llamar a funcion con funcion fuera de componente
             </button>
         </>
     )
