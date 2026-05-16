f4549e436c80c0eebfa764e13655a5728a085fe0
Cesar Morales Dev <cesar.morales.dev@gmail.com>
2026-05-16 17:21:40 -0500

    165.- Contextos y proveedores en React

--- 04-hooks-app/src/09-useContext/pages/about/AboutPage.tsx
+++ 04-hooks-app/src/09-useContext/pages/about/AboutPage.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { Link } from "react-router"
 
 export const AboutPage = () => {
     return (
-        <div>AboutPage</div>
+        <div className='flex flex-col items-center justify-center min-h-screen'>
+            <h1 className='text-4xl font-bold'>Pagina sobre mi</h1>
+            <hr />
+            <div className="flex flex-col gap-2">
+                <Link to='/profile' className="hover:text-blue-500 underline text-2xl">Perfil</Link>
+                <Link to='/auth' className="hover:text-blue-500 underline text-2xl">Iniciar sesion</Link>
+            </div>
+        </div>
     )
 }
