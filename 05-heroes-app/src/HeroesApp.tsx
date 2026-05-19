424ec87525433ce5d30f4fd3ca2bcd2c54cc4621
Cesar Morales <cesar.aml1891@gmail.com>
2026-05-18 21:52:26 -0500

    175.- Creacion de proyecto - HeroesApp

---
+++ 05-heroes-app/src/HeroesApp.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+export const HeroesApp = () => {
+    return (
+        <div>
+            <h1>
+                Hola mundo
+            </h1>
+            <Button>Hola mundo</Button>
+        </div>
+    )
+}
