f8a63f822e518e193101eefc8a0d93593f69262f
Cesar Morales Dev <cesar.morales.dev@gmail.com>
2026-06-09 01:04:35 -0500

    181.- Parte 2 - Pensemos en componentes

--- 05-heroes-app/src/heroes/pages/search/SearchPage.tsx
+++ 05-heroes-app/src/heroes/pages/search/SearchPage.tsx
@@ -1,9 +1,11 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
+import { HeroStats } from "@/heroes/components/HeroStats";
 
 const SearchPage = () => {
   return (
     <>
       <CustomJumbotron title="Busqueda de Superhero" description="Discover, explore, and manage your favorite superheroes and villains" />
+      <HeroStats />
     </>
   )
 }
