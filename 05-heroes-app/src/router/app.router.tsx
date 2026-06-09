72039eb0c4d697e6508e11869104db85c81f7046
Cesar Morales Dev <cesar.morales.dev@gmail.com>
2026-06-08 23:43:25 -0500

    169.- Componente Layout

--- 05-heroes-app/src/router/app.router.tsx
+++ 05-heroes-app/src/router/app.router.tsx
@@ -1,6 +1,6 @@
-import { AdminLayout } from "@/admin/layout/AdminLayout";
+import { AdminLayout } from "@/admin/layouts/AdminLayout";
 import { AdminPage } from "@/admin/pages/AdminPage";
-import { HeroesLayout } from "@/heroes/layout/HeroesLayout";
+import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
 import { HeroPage } from "@/heroes/pages/hero/HeroPage";
 import { HomePage } from "@/heroes/pages/home/HomePage";
 import { SearchPage } from "@/heroes/pages/search/SearchPage";
